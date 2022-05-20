5c4e72462a987e2c1590c40e4f91ebe312a71d14
Vikas Yadav <v.ray96610@gmail.com>
2022-05-20 19:59:58 +0530

    3 new services

---
+++ movie-catlog-service/src/main/java/com/jvmchain/micro/moviecatlogservice/MovieCatlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jvmchain.micro.moviecatlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatlogServiceApplication.class, args);
+	}
+
+}
---
+++ movie-catlog-service/src/test/java/com/jvmchain/micro/moviecatlogservice/MovieCatlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmchain.micro.moviecatlogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
